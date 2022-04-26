2e7a9fd699a3e441d4fb1e6ae7b856483a8259ce
Yitzchak Lewkowicz <yitzchaklewkow@yahoo.com>
2022-04-26 11:42:44 -0600

    commit tree 8026092663cbf83ef32bb853e133dc2918739a32

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your solution in this file!
+//const employee = {name:'Sam'};
+
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    employee.key = value;
+    return employee;
+}
